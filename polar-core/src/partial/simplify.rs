26b1f3446bf2ea70df8611e7b9c6101e13bef1b6
Gabe Jackson <gj@mail.co.de>
2020-10-21 18:29:53 -0400

    A partial with no constraints resolves to true

--- polar-core/src/partial/simplify.rs
+++ polar-core/src/partial/simplify.rs
@@ -30,6 +30,10 @@ pub struct Simplifier;
 impl Folder for Simplifier {
     fn fold_term(&mut self, t: Term) -> Term {
         match t.value() {
+            Value::Partial(Constraints { operations, .. }) if operations.is_empty() => {
+                t.clone_with_value(Value::Boolean(true))
+            }
+
             Value::Partial(Constraints { operations, .. }) if operations.len() == 1 => {
                 fn is_this_arg(t: &Term) -> bool {
                     matches!(t.value(), Value::Variable(v) if v.is_this_var())
@@ -53,9 +57,11 @@ impl Folder for Simplifier {
                         assert_eq!(args.len(), 1);
                         args.pop().unwrap()
                     }
+
                     _ => fold_term(t, self),
                 }
             }
+
             _ => fold_term(t, self),
         }
     }
